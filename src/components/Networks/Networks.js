df0f6e04d0857e84fd700fe2443c68ee490635f0
linkednode <linked.node09@gmail.com>
2024-05-12 17:47:56 +0700

    add airchain, galactica, and og labs

--- src/components/Networks/Networks.js
+++ src/components/Networks/Networks.js
@@ -15,6 +15,9 @@ import lava from "../../Assets/Networks/lava.png"
 import bevm from "../../Assets/Networks/bevm.png"
 import autonity from "../../Assets/Networks/autonity.jpg"
 import warden from "../../Assets/Networks/warden.jpeg"
+import oglabs from "../../Assets/Networks/oglabs.jpg"
+import galactica from "../../Assets/Networks/galactica.jpg"
+import airchains from "../../Assets/Networks/airchains.jpg"
 
 import Button from 'react-bootstrap/Button';
 
@@ -50,6 +53,26 @@ function Networks() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <NetworksCard
+              imgPath={oglabs}
+              isHaveExplorer={true}
+              title="0glabs"
+              description=""
+              websiteLink="https://0g.ai/"
+              explorerLink="https://testnet.linkednode.xyz/0g/staking/0gvaloper132tnykv52dh2z4z2xs0tvxtgqy6eq4udrn8qjn"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <NetworksCard
+              imgPath={airchains}
+              isHaveExplorer={true}
+              title="Airchains"
+              description=""
+              websiteLink="https://www.airchains.io/"
+              explorerLink="https://testnet.linkednode.xyz/airchains/staking/airvaloper12z70y9wky62y686v64xrs6e8vkzxgzsuzesf8q"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <NetworksCard
               imgPath={artela}
               isHaveExplorer={true}
               title="Artela"
@@ -73,42 +96,12 @@ function Networks() {
           </Col>
           <Col md={4} className="project-card">
             <NetworksCard
-              imgPath={entangle}
+              imgPath={galactica}
               isHaveExplorer={true}
-              title="Entangle"
+              title="Galactica"
               description=""
-              websiteLink="https://entangle.fi"
-              explorerLink="https://testnet.linkednode.xyz/entangle/staking/ethmvaloper1k9zes784hjml8ktdyzussxaqrrvlnec5jsjt4f"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <NetworksCard
-              imgPath={lava}
-              isHaveExplorer={true}
-              title="Lava"
-              description=""
-              websiteLink="https://www.lavanet.xyz"
-              explorerLink="https://testnet.linkednode.xyz/lava/staking/lava@valoper18e5r6pec60lsxmakajsgh7nc8e4tl5lnkup8j5"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <NetworksCard
-              imgPath={pryzm}
-              isHaveExplorer={true}
-              title="Pryzm"
-              description=""
-              websiteLink="https://pryzm.zone"
-              explorerLink="https://testnet.linkednode.xyz/pryzm/staking/pryzmvaloper1rpx9jms35nk7t8rdjgscv49tku6q4nupkc5tje"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <NetworksCard
-              imgPath={selfchain}
-              isHaveExplorer={true}
-              title="Selfchain"
-              description=""
-              websiteLink="https://selfchain.xyz"
-              explorerLink="https://testnet.linkednode.xyz/selfchain/staking/selfvaloper1v7zaq024vaykn8v3cqnyj0v0jjyd34eq95042l"
+              websiteLink="https://galactica.com/"
+              explorerLink="https://testnet.linkednode.xyz/galactica/staking/galavaloper1psc65ql4pu6j6dhfu2zrzn584djwhthhz80del"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -119,16 +112,6 @@ function Networks() {
               description=""
               websiteLink="https://wardenprotocol.org"
               explorerLink="https://testnet.linkednode.xyz/warden/staking/wardenvaloper1g8ggpa8t99qyrzyldvcmmy5p2p44n44el0290r"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <NetworksCard
-              imgPath={autonity}
-              isHaveExplorer={true}
-              title="Autonity"
-              description="Piccadilly Testnet R5"
-              websiteLink="https://game.autonity.org"
-              explorerLink="https://stakeflow.io/autonity-piccadilly/validators/0xc7a7c5314b924d442787888fcdbb0dfbd9123a27"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -151,23 +134,13 @@ function Networks() {
               explorerLink="https://auroria.beacon.stratisevm.com/validator/b520077ea1d923f9a70851c249c7830dd5181715a669c05b8bbeaa9e1db1c7f228838d9ea1106a0447d9c0f9c29c5faf"
             />
           </Col>
-          <Col md={4} className="project-card">
-            <NetworksCard
-              imgPath={nulink}
-              isHaveExplorer={false}
-              title="Nulink"
-              description="Horus2.0 Testnet"
-              websiteLink="https://nulink.org"
-              explorerLink=""
-            />
-          </Col>
         </Row>
       <h2 className="logo-heading">
         Archive
       </h2>
       {/* <div className="d-grid gap-2">
         <Button variant="secondary" size="lg">
-          N/A
+        N/A
         </Button>
       </div> */}
       <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
@@ -179,6 +152,66 @@ function Networks() {
             description=""
             websiteLink=""
             explorerLink=""
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={nulink}
+            isHaveExplorer={false}
+            title="Nulink"
+            description="Horus2.0 Testnet"
+            websiteLink="https://nulink.org"
+            explorerLink=""
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={entangle}
+            isHaveExplorer={true}
+            title="Entangle"
+            description=""
+            // websiteLink="https://entangle.fi"
+            // explorerLink="https://testnet.linkednode.xyz/entangle/staking/ethmvaloper1k9zes784hjml8ktdyzussxaqrrvlnec5jsjt4f"
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={lava}
+            isHaveExplorer={true}
+            title="Lava"
+            description=""
+            // websiteLink="https://www.lavanet.xyz"
+            // explorerLink="https://testnet.linkednode.xyz/lava/staking/lava@valoper18e5r6pec60lsxmakajsgh7nc8e4tl5lnkup8j5"
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={pryzm}
+            isHaveExplorer={true}
+            title="Pryzm"
+            description=""
+            // websiteLink="https://pryzm.zone"
+            // explorerLink="https://testnet.linkednode.xyz/pryzm/staking/pryzmvaloper1rpx9jms35nk7t8rdjgscv49tku6q4nupkc5tje"
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={selfchain}
+            isHaveExplorer={true}
+            title="Selfchain"
+            description=""
+            // websiteLink="https://selfchain.xyz"
+            // explorerLink="https://testnet.linkednode.xyz/selfchain/staking/selfvaloper1v7zaq024vaykn8v3cqnyj0v0jjyd34eq95042l"
+          />
+        </Col>
+        <Col md={4} className="project-card">
+          <NetworksCard
+            imgPath={autonity}
+            isHaveExplorer={true}
+            title="Autonity"
+            description="Piccadilly Testnet R5"
+            // websiteLink="https://game.autonity.org"
+            // explorerLink="https://stakeflow.io/autonity-piccadilly/validators/0xc7a7c5314b924d442787888fcdbb0dfbd9123a27"
           />
         </Col>
       </Row>
